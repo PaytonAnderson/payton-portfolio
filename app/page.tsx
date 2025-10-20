5df1def60eed1b2670b8f6ed78bb07f8dc7269de
PaytonAnderson <payton@clickityclick.com>
2025-10-20 11:15:05 -0700

    changed font size

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <>
-      <main className="flex flex-col gap-2 flex-1 items-stretch ">
+      <main className="flex flex-col sm:flex-row gap-2 flex-1 items-stretch ">
         <Link href={"/projects"}>
           <Button className="w-full">Projects</Button>
         </Link>
         <Link href={"/about"}>
           <Button className="w-full">About Me</Button>
+        </Link>
+        <Link href={"/education"}>
+          <Button className="w-full">Education</Button>
         </Link>
         <Link href={"/contact"}>
           <Button className="w-full">Contact Me</Button>
